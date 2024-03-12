fabcfbef8361315952e023fb7129714cacb07044
Malte Bastian <40334865+bc-m@users.noreply.github.com>
2024-03-12 19:16:07 +0100

    feat: parse custom menu set catalog (#16)

---
+++ src/custom_menu_set_catalog.rs
@@ -0,0 +1,52 @@
+use quick_xml::events::{BytesStart, Event};
+use quick_xml::reader::Reader;
+use std::io::{BufRead, Read};
+use std::path::Path;
+
+use crate::utils::xml_utils::skip_element;
+use crate::utils::{initialize_out_dir, write_xml_element_to_file};
+
+pub fn xml_explode_custom_menu_set_catalog<R: Read + BufRead>(
+    reader: &mut Reader<R>,
+    _: &BytesStart,
+    out_dir_path: &Path,
+    fm_file_name: &str,
+) {
+    let out_dir_path = out_dir_path.join("custom_menu_sets").join(fm_file_name);
+    initialize_out_dir(&out_dir_path);
+
+    let mut depth = 1;
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Err(e) => {
+                println!("Error {}", e);
+                break;
+            }
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e)) => {
+                depth += 1;
+                if depth == 3 {
+                    if e.name().as_ref() == b"CustomMenuSet" {
+                        write_xml_element_to_file(reader, &e, &out_dir_path, 5);
+                        depth -= 1;
+                        continue;
+                    } else {
+                        skip_element(reader, &e);
+                        depth -= 1;
+                        continue;
+                    }
+                }
+            }
+            Ok(Event::End(_)) => {
+                depth -= 1;
+                if depth == 0 {
+                    break;
+                }
+            }
+            _ => {}
+        }
+
+        buf.clear()
+    }
+}
