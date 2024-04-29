647d1a5036c46bb3f5246b944e958e2769a324d4
Malte Bastian <m.bastian@mikelcom.de>
2024-04-29 23:23:56 +0200

    refactor: use FromRepr instead EnumString for enum simplification

--- src/script_steps/constants.rs
+++ src/script_steps/constants.rs
@@ -1,177 +1,94 @@
-use std::str::FromStr;
-use strum_macros::EnumString;
+use strum_macros::FromRepr;
 
-#[derive(Debug, EnumString, PartialEq)]
+const UNKNOWN_SCRIPT_STEP: [u32; 16] = [
+    2, 3, 15, 52, 53, 54, 58, 100, 110, 162, 163, 170, 171, 173, 198, 204,
+];
+
+#[derive(Debug, FromRepr, PartialEq)]
+#[repr(u32)]
 pub enum ScriptStep {
-    #[strum(
-        serialize = "2",
-        serialize = "3",
-        serialize = "15",
-        serialize = "52",
-        serialize = "53",
-        serialize = "54",
-        serialize = "58",
-        serialize = "100",
-        serialize = "110",
-        serialize = "162",
-        serialize = "163",
-        serialize = "170",
-        serialize = "171",
-        serialize = "173",
-        serialize = "198",
-        serialize = "204"
-    )]
-    Unknown,
-    #[strum(serialize = "1")]
-    PerformScript,
-    #[strum(serialize = "4")]
-    GoToPreviousField,
-    #[strum(serialize = "5")]
-    GoToNextField,
-    #[strum(serialize = "6")]
-    GoToLayout,
-    #[strum(serialize = "7")]
-    NewRecordRequest,
-    #[strum(serialize = "8")]
-    DuplicateRecordRequest,
-    #[strum(serialize = "9")]
-    DeleteRecordRequest,
-    #[strum(serialize = "10")]
-    DeleteAllRecords,
-    #[strum(serialize = "16")]
-    GoToRecordRequestPage,
-    #[strum(serialize = "17")]
-    GoToField,
-    #[strum(serialize = "19")]
-    CheckRecord,
-    #[strum(serialize = "20")]
-    CheckFoundSet,
-    #[strum(serialize = "21")]
-    UnsortRecords,
-    #[strum(serialize = "22")]
-    EnterFindMode,
-    #[strum(serialize = "23")]
-    ShowAllRecords,
-    #[strum(serialize = "24")]
-    ModifyLastFind,
-    #[strum(serialize = "25")]
-    OmitRecord,
-    #[strum(serialize = "26")]
-    OmitMultipleRecords,
-    #[strum(serialize = "27")]
-    ShowOmittedOnly,
-    #[strum(serialize = "28")]
-    PerformFind,
-    #[strum(serialize = "32")]
-    OpenHelp,
-    #[strum(serialize = "38")]
-    OpenManageDatabase,
-    #[strum(serialize = "44")]
-    ExitApplication,
-    #[strum(serialize = "50")]
-    SelectAll,
-    #[strum(serialize = "55")]
-    EnterBrowseMode,
-    #[strum(serialize = "68")]
-    IfStart,
-    #[strum(serialize = "69")]
-    Else,
-    #[strum(serialize = "70")]
-    IfEnd,
-    #[strum(serialize = "71")]
-    LoopStart,
-    #[strum(serialize = "72")]
-    ExitLoopIf,
-    #[strum(serialize = "73")]
-    LoopEnd,
-    #[strum(serialize = "75")]
-    CommitRecordRequests,
-    #[strum(serialize = "76")]
-    SetFieldData,
-    #[strum(serialize = "79")]
-    FixWindow,
-    #[strum(serialize = "82")]
-    NewFile,
-    #[strum(serialize = "85")]
-    AllowUserAbort,
-    #[strum(serialize = "86")]
-    SetErrorRecording,
-    #[strum(serialize = "88")]
-    OpenScriptWorkspace,
-    #[strum(serialize = "89")]
-    Comment,
-    #[strum(serialize = "90")]
-    HaltScript,
-    #[strum(serialize = "91")]
-    ReplaceFieldContents,
-    #[strum(serialize = "93")]
-    Beep,
-    #[strum(serialize = "94")]
-    SetUseSystemFormats,
-    #[strum(serialize = "99")]
-    GoToPortalRow,
-    #[strum(serialize = "101")]
-    CopyRecordRequest,
-    #[strum(serialize = "102")]
-    FlushCacheToDisk,
-    #[strum(serialize = "103")]
-    ExitScript,
-    #[strum(serialize = "105")]
-    OpenSettings,
-    #[strum(serialize = "106")]
-    CorrectWord,
-    #[strum(serialize = "107")]
-    SpellingOptions,
-    #[strum(serialize = "108")]
-    SelectDictionaries,
-    #[strum(serialize = "109")]
-    EditUserDictionary,
-    #[strum(serialize = "112")]
-    OpenManageValueLists,
-    #[strum(serialize = "113")]
-    OpenSharing,
-    #[strum(serialize = "114")]
-    OpenFileOptions,
-    #[strum(serialize = "115")]
-    AllowFormattingBar,
-    #[strum(serialize = "118")]
-    OpenHosts,
-    #[strum(serialize = "121")]
-    CloseWindow,
-    #[strum(serialize = "122")]
-    NewWindow,
-    #[strum(serialize = "125")]
-    IfElse,
-    #[strum(serialize = "126")]
-    ConstrainFoundSet,
-    #[strum(serialize = "127")]
-    ExtendFoundSet,
-    #[strum(serialize = "129")]
-    OpenFindReplace,
-    #[strum(serialize = "140")]
-    OpenManageDataSources,
-    #[strum(serialize = "141")]
-    SetVariable,
-    #[strum(serialize = "145")]
-    GoToObject,
-    #[strum(serialize = "149")]
-    OpenEditSavedFinds,
-    #[strum(serialize = "151")]
-    OpenManageLayouts,
-    #[strum(serialize = "156")]
-    OpenManageContainers,
-    #[strum(serialize = "165")]
-    OpenManageThemes,
-    #[strum(serialize = "167")]
-    RefreshObject,
-    #[strum(serialize = "169")]
-    ClosePopover,
-    #[strum(serialize = "172")]
-    UploadToServer,
-    #[strum(serialize = "183")]
-    OpenMyApps,
+    Unknown = 0,
+    PerformScript = 1,
+    GoToPreviousField = 4,
+    GoToNextField = 5,
+    GoToLayout = 6,
+    NewRecordRequest = 7,
+    DuplicateRecordRequest = 8,
+    DeleteRecordRequest = 9,
+    DeleteAllRecords = 10,
+    GoToRecordRequestPage = 16,
+    GoToField = 17,
+    CheckRecord = 19,
+    CheckFoundSet = 20,
+    UnsortRecords = 21,
+    EnterFindMode = 22,
+    ShowAllRecords = 23,
+    ModifyLastFind = 24,
+    OmitRecord = 25,
+    OmitMultipleRecords = 26,
+    ShowOmittedOnly = 27,
+    PerformFind = 28,
+    OpenHelp = 32,
+    OpenManageDatabase = 38,
+    ExitApplication = 44,
+    SelectAll = 50,
+    EnterBrowseMode = 55,
+    IfStart = 68,
+    Else = 69,
+    IfEnd = 70,
+    LoopStart = 71,
+    ExitLoopIf = 72,
+    LoopEnd = 73,
+    CommitRecordRequests = 75,
+    SetFieldData = 76,
+    FixWindow = 79,
+    NewFile = 82,
+    AllowUserAbort = 85,
+    SetErrorRecording = 86,
+    OpenScriptWorkspace = 88,
+    Comment = 89,
+    HaltScript = 90,
+    ReplaceFieldContents = 91,
+    Beep = 93,
+    SetUseSystemFormats = 94,
+    GoToPortalRow = 99,
+    CopyRecordRequest = 101,
+    FlushCacheToDisk = 102,
+    ExitScript = 103,
+    OpenSettings = 105,
+    CorrectWord = 106,
+    SpellingOptions = 107,
+    SelectDictionaries = 108,
+    EditUserDictionary = 109,
+    OpenManageValueLists = 112,
+    OpenSharing = 113,
+    OpenFileOptions = 114,
+    AllowFormattingBar = 115,
+    OpenHosts = 118,
+    CloseWindow = 121,
+    NewWindow = 122,
+    IfElse = 125,
+    ConstrainFoundSet = 126,
+    ExtendFoundSet = 127,
+    OpenFindReplace = 129,
+    OpenManageDataSources = 140,
+    SetVariable = 141,
+    GoToObject = 145,
+    OpenEditSavedFinds = 149,
+    OpenManageLayouts = 151,
+    OpenManageContainers = 156,
+    OpenManageThemes = 165,
+    RefreshObject = 167,
+    ClosePopover = 169,
+    UploadToServer = 172,
+    OpenMyApps = 183,
 }
 
 pub fn id_to_script_step(id: &str) -> ScriptStep {
-    ScriptStep::from_str(id).unwrap_or(ScriptStep::Unknown)
+    let id = id.parse::<u32>().unwrap();
+    if UNKNOWN_SCRIPT_STEP.contains(&id) {
+        ScriptStep::Unknown
+    } else {
+        ScriptStep::from_repr(id).unwrap_or(ScriptStep::Unknown)
+    }
 }
